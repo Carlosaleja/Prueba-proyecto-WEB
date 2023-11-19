6262669ecdc5d35bf81eddafcbdf62de5d8ccb25
luisnativii <00200022@uca.edu.sv>
2023-11-19 10:23:00 -0500

    merge manueal de diseño con express

---
+++ Hechame-la-mano-client/src/pages/Form_ayuda.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Inicio.css";
+import Header from "../assets/Header";
+
+export const Form_ayuda = () => {
+    return (
+        <div>
+            
+            <Header className="header-instance" logo="image.png" property1="default" />
+            <h1>Formas de ayudar </h1>
+        </div>
+    );
+};
+
+
+export default Form_ayuda;
